6e1c43d53b0d381086cf3be274f6c24fd8cc9e1c
MOHD AZAMUDDIN <77785226+deathstroke235@users.noreply.github.com>
2021-06-01 17:59:23 +0530

    Create rotatinganarray.java

---
+++ Programs/rotatinganarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+       for(int i = 0; i < k; i++){    
+            int j, last;        
+            last = nums[nums.length-1];    
+            
+            for(j = nums.length-1; j > 0; j--){    
+                   
+                nums[j] = nums[j-1];    
+            }      
+            nums[0] = last;    
+        }    
+    }
+}
