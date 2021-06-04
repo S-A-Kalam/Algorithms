374828ae483e42c185537c9f0d751e14461a569e
MOHD AZAMUDDIN <77785226+deathstroke235@users.noreply.github.com>
2021-06-04 12:33:01 +0530

    Create uniquecharecter.java

---
+++ Programs/uniquecharecter.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] h=new int[26];
+        for(int i=0;i<s.length();i++){
+            h[s.charAt(i)-97]+=1;
+        }
+        for(int i=0;i<s.length();i++){
+           if(h[s.charAt(i)-97]==1)
+               return i;
+           
+        }
+        return -1;
+    }
+}
